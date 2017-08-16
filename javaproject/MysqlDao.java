68b24e44842cb89585e73acecd1604c604fdcdaf
liaochengqian <chengqian.liao@qq.com>
2017-08-16 20:46:31 +0800

    modified:   javaproject/MysqlDao.java 	modified:   javaproject/TestDao.java

--- javaproject/MysqlDao.java
+++ javaproject/MysqlDao.java
@@ -1,15 +1,49 @@
-public class MysqlDao{
-    private static final DRIVER = "com.mysql.jdbc.Driver"
-    private static final URL = "jdbc:mysql://localhost:3306/ltd0815";
-    private static final USER = "root";
-    private static final PASSWD = "";
+package com.jdbc;
 
+import java.sql.*;
 
-    public static Connection getConnect(){
-        Class.forName( Driver );
+public class MysqlDao {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private Connection connection;
+    private String url = null;
+    private String user = null;
+    private String passwd = null;
 
+    public MysqlDao(String url, String user, String passwd) {
+        this.url = url;
+        this.user = user;
+        this.passwd = passwd;
     }
 
+    public Connection getConnect() {
+        if (connection == null) {
+            try {
+                Class.forName(DRIVER);
+                connection = DriverManager.getConnection(url, user, passwd);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 
+    public void getClosed() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    //preparedstatment sentence
+    public boolean change(String sql , Object ... parameters ) throws SQLException{
+        PreparedStatement pre = connection.prepareStatement(sql);
+        for( int i =0 ; i< parameters.length ; i++){
+            System.out.println( parameters[i] );
+            pre.setObject( i+1 , parameters[i] );
+        }
+        return pre.executeUpdate() > 0 ? true : false;
+    }
 }
--- javaproject/TestDao.java
+++ javaproject/TestDao.java
@@ -1,5 +1,36 @@
-public class TestDao {
-    public static void main(String[] args){
+package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestDao {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost/ltd0815";
+        String user = "root";
+        String passwd = "";
+        String table = "staff";
+
+        MysqlDao dao = new MysqlDao(url,user,passwd);
+        Connection conn = dao.getConnect();
+
+        //必须将sql语句打印出来，极大的有助于发现错误
+        String sql1 = "insert into " + table + " values(?,?,?,?,?,?)";
+        System.out.println( "---sql:=" + sql1 );
+
+        dao.change( sql1 , 2016080360,"张88","男","1989/12/11",104.01,"10002" );
+
+        Statement s = conn.createStatement();
+        String sql = "select * from staff";
+        ResultSet res = s.executeQuery( sql );
+        System.out.println("\tno\tname\t\tdept");
+        while (res.next()) {
+            int no = res.getInt("sno");
+            String name = res.getString("sname");
+            String dep = res.getString("sdept");
+            System.out.printf("%d\t%s\t\t%s\n", no, name, dep);
+        }
+        dao.getClosed();
     }
 }
